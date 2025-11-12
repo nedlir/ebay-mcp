7f0abd21f93850690d120718d391cdbb237cb768
YosefHayim <yosefisabag+03@gmail.com>
2025-11-12 11:55:25 +0200

    .

--- scripts/create-token-template.sh
+++ scripts/create-token-template.sh
@@ -61,11 +61,11 @@ echo ""
 
 cat > "$TOKEN_FILE" << 'EOF'
 {
-  "accessToken": "YOUR_USER_ACCESS_TOKEN",
-  "refreshToken": "YOUR_USER_REFRESH_TOKEN",
+  "userAccessToken": "YOUR_USER_ACCESS_TOKEN",
+  "userRefreshToken": "YOUR_USER_REFRESH_TOKEN",
   "tokenType": "Bearer",
-  "accessTokenExpiry": 1767225600000,
-  "refreshTokenExpiry": 1825363200000,
+  "userAccessTokenExpiry": 1767225600000,
+  "userRefreshTokenExpiry": 1825363200000,
   "scope": "https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.notification.subscription https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly https://api.ebay.com/oauth/api_scope/sell.stores https://api.ebay.com/oauth/api_scope/sell.stores.readonly"
 }
 EOF
